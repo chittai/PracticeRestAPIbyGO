4a37d79574da00939af99a9a60fc2b07211c7f9d
布田遼平 <chittai1221@gmail.com>
2019-04-26 02:27:05 +0900

    add db

--- main.go
+++ main.go
@@ -3,7 +3,14 @@ package main
 import(
 	"fmt"
 	"time"
+	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+    "github.com/aws/aws-sdk-go/aws/credentials"
+    "github.com/aws/aws-sdk-go/aws/session"
+    "github.com/guregu/dynamo"
 	"github.com/rs/xid"
+	"github.com/joho/godotenv"
 )
 
 type User struct {
@@ -12,11 +19,31 @@ type User struct {
 	CreatedTime	time.Time	`dynamo:"created_time"`
 }
 
+
+
 func main(){
+	godotenv.Load("./envfiles/develop.env")
+
+	var apikey = os.Getenv("APIKEY")
+	var secretkey = os.Getenv("SECRETKEY")
+
+	var c = credentials.NewStaticCredentials(apikey, secretkey, "")
+
+	var db = dynamo.New(session.New(), &aws.Config{
+		Credentials: c,
+		Region:	aws.String("us-east-1"),
+	})
+	
+	var table = db.Table("Test")
+
 	guid := xid.New()
 	name := "taro"
 
 	u := User{UserID: guid, Name: name, CreatedTime: time.Now().UTC()}
 	fmt.Println(u)
-}
 
+	if err := table.Put(u).Run(); err != nil {
+		fmt.Println("err")
+		panic(err.Error())
+	}
+}
